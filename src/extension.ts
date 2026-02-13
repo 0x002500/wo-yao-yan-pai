1e3c6525c19b63efd866b4882965b13cdabab20c
0x002500 <0x002500@0x0025.eu.org>
2026-02-13 14:18:42 +0800

    feat: add basic code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
+const player = require("play-sound")({});
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  function playSound(fileName: string) {
+    const soundPath = path.join(context.extensionPath, "media", fileName);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wo-yao-yan-pai" is now active!');
+    player.play(soundPath, (err: any) => {
+      if (err) {
+        console.error("Error playing sound:", err);
+      }
+    });
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('wo-yao-yan-pai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from wo-yao-yan-pai!');
-	});
+  vscode.tasks.onDidStartTask(() => {
+    playSound("start.wav");
+  });
 
-	context.subscriptions.push(disposable);
+  vscode.tasks.onDidEndTaskProcess((event) => {
+    if (event.exitCode === 0) {
+      playSound("success.wav");
+    } else {
+      playSound("fail.wav");
+    }
+  });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
