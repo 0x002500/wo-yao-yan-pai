33e5b6d33813e54c50f5667ef5f49a941d99b6db
0x002500 <0x002500@0x0025.eu.org>
2026-02-13 14:21:43 +0800

    feat: support toggle the sound and add some comment

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,26 @@ import * as path from "path";
 const player = require("play-sound")({});
 
 export function activate(context: vscode.ExtensionContext) {
+  let enabled = context.globalState.get<boolean>("buildSoundEnabled", true);
+
+  // 创建状态栏按钮
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100,
+  );
+
+  function updateStatusBar() {
+    statusBarItem.text = enabled ? "我要验牌: 开" : "🔇 我要验牌: 关";
+    statusBarItem.tooltip = "点击按钮打开或关闭法(fà)国赌神";
+    statusBarItem.command = "buildSound.toggle";
+    statusBarItem.show();
+  }
+
   function playSound(fileName: string) {
+    if (!enabled) {
+      return;
+    }
+
     const soundPath = path.join(context.extensionPath, "media", fileName);
 
     player.play(soundPath, (err: any) => {
@@ -13,17 +32,33 @@ export function activate(context: vscode.ExtensionContext) {
     });
   }
 
-  vscode.tasks.onDidStartTask(() => {
+  // 注册开关命令
+  const toggleCommand = vscode.commands.registerCommand(
+    "buildSound.toggle",
+    () => {
+      enabled = !enabled;
+      context.globalState.update("buildSoundEnabled", enabled);
+      updateStatusBar();
+    },
+  );
+
+  // 编译开始
+  vscode.tasks.onDidStartTask((event: vscode.TaskStartEvent) => {
     playSound("start.wav");
   });
 
-  vscode.tasks.onDidEndTaskProcess((event) => {
+  // 编译结束
+  vscode.tasks.onDidEndTaskProcess((event: vscode.TaskProcessEndEvent) => {
     if (event.exitCode === 0) {
       playSound("success.wav");
     } else {
       playSound("fail.wav");
     }
   });
+
+  updateStatusBar();
+
+  context.subscriptions.push(statusBarItem, toggleCommand);
 }
 
 export function deactivate() {}
